909e22bd51ff97c1e8c242eb8d898609d39e0f4e
21ABBMax <92145624+21ABBMax@users.noreply.github.com>
2024-04-23 16:04:12 +0200

    Fixed the error message

--- Noob-if-you-look/checkMatch.js
+++ Noob-if-you-look/checkMatch.js
@@ -1,21 +1,46 @@
-const usernameInput = document.getElementById("usernameInput");
-const passwordInput = document.getElementById("passwordInput");
+import { fetchData } from "../firebase.js";
 
-export function checkForMatch(username, password, users) {
-	console.log("Received username:", username);
-	console.log("Received password:", password);
+function toggleClass(elementId, className) {
+	const element = document.getElementById(elementId);
+	if (!element.classList.contains(className)) {
+		element.classList.add(className);
+	}
+}
+
+function removeClass(elementId, className) {
+	const element = document.getElementById(elementId);
+	if (element.classList.contains(className)) {
+		element.classList.remove(className);
+	}
+}
+
+export async function checkForMatch() {
+	const users = await fetchData();
+
+	const usernameInput = document.getElementById("usernameInput").value;
+	const passwordInput = document.getElementById("passwordInput").value;
+
+	console.log("Received username:", usernameInput);
+	console.log("Received password:", passwordInput);
 	console.log("Fetched users data:", users);
 
 	// Convert the received password to a string if it's not already
-	const receivedPassword = String(password);
+	const receivedPassword = String(passwordInput);
 
 	// Convert the stored password to a string
-	const storedPassword = users.hasOwnProperty(username)
-		? String(users[username])
-		: null;
+	const storedPassword = users.hasOwnProperty(usernameInput)
+		? String(users[usernameInput])
+		: "";
 
 	// Check if the username and password match a specific key-value pair in the users object
 	const match = storedPassword && storedPassword === receivedPassword;
+
+	if (match) {
+		removeClass("errorP", "open");
+	} else {
+		toggleClass("errorP", "open");
+	}
+
 	console.log("Match result:", match);
 
 	// Log keys and values of the users object for further inspection
@@ -25,3 +50,5 @@ export function checkForMatch(username, password, users) {
 
 	return match;
 }
+
+window.checkForMatch = checkForMatch;
